21e6c07280de409a5819e163af40283076909dce
koffeinfrei <none@none>
2010-10-02 01:43:39 +0200

    * added map
    --HG--
    rename : org.koffeinfrei.zueribad/res/layout/details_section_content.xml => org.koffeinfrei.zueribad/res/layout/details_section_content_general.xml

--- org.koffeinfrei.zueribad/gen/org/koffeinfrei/zueribad/R.java
+++ org.koffeinfrei.zueribad/gen/org/koffeinfrei/zueribad/R.java
@@ -25,25 +25,27 @@ public final class R {
     public static final class id {
         public static final int details_explist=0x7f050002;
         public static final int details_picture=0x7f050001;
-        public static final int details_section_content1=0x7f050003;
-        public static final int details_section_content2=0x7f050004;
-        public static final int details_section_title=0x7f050005;
+        public static final int details_section_content_general1=0x7f050003;
+        public static final int details_section_content_general2=0x7f050004;
+        public static final int details_section_content_map=0x7f050005;
+        public static final int details_section_title=0x7f050006;
         public static final int details_title=0x7f050000;
-        public static final int list=0x7f050007;
-        public static final int overview_listitem_bathname=0x7f050009;
-        public static final int overview_listitem_bathtemperature=0x7f050008;
-        public static final int overview_listitem_bathtype=0x7f05000a;
-        public static final int search_box=0x7f050006;
+        public static final int list=0x7f050008;
+        public static final int overview_listitem_bathname=0x7f05000a;
+        public static final int overview_listitem_bathtemperature=0x7f050009;
+        public static final int overview_listitem_bathtype=0x7f05000b;
+        public static final int search_box=0x7f050007;
     }
     public static final class layout {
         public static final int details=0x7f030000;
-        public static final int details_section_content=0x7f030001;
-        public static final int details_section_title=0x7f030002;
-        public static final int favorites=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int nearlocation=0x7f030005;
-        public static final int overview=0x7f030006;
-        public static final int overview_listview=0x7f030007;
+        public static final int details_section_content_general=0x7f030001;
+        public static final int details_section_content_map=0x7f030002;
+        public static final int details_section_title=0x7f030003;
+        public static final int favorites=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int nearlocation=0x7f030006;
+        public static final int overview=0x7f030007;
+        public static final int overview_listview=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/DetailsActivity.java
+++ org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/DetailsActivity.java
@@ -1,13 +1,20 @@
 package org.koffeinfrei.zueribad;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import org.koffeinfrei.zueribad.models.BathRepository;
 import org.koffeinfrei.zueribad.models.entities.Bath;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -22,6 +29,8 @@ public class DetailsActivity extends Activity {
 		setContentView(R.layout.details);
 		
 		TextView titleView = (TextView)findViewById(R.id.details_title);
+		ImageView pictureView = (ImageView) findViewById(R.id.details_picture);
+		MapView mapView = (MapView) findViewById(R.id.details_section_content_map);
 		
 		Bundle extras = getIntent().getExtras();
 		if(extras != null)
@@ -29,36 +38,59 @@ public class DetailsActivity extends Activity {
 			int id = extras.getInt("SelectedItemId"); // TODO ->constant
 			Bath bath = BathRepository.getInstance().get(id);
 			
+			// title
 			titleView.setText(bath.getName());
 			
-			ImageView pictureView = (ImageView) findViewById(R.id.details_picture);
+			// picture
 			pictureView.setImageBitmap(bath.getPicture());
+			
+			// map
+			Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+			try {
+				List<Address> addresses = geocoder.getFromLocationName("Wasserwerkstrasse 141, 8037 Zürich", 1);
+				
+				if (!addresses.isEmpty()){
+					Address address = addresses.get(0);
+					GeoPoint geoPoint = new GeoPoint(
+							(int)(address.getLatitude() * 1000000.0), 
+							(int)(address.getLongitude() * 1000000.0));
+					mapView.getController().setCenter(geoPoint);
+					mapView.setBuiltInZoomControls(true);
+				}
+				else{
+					System.err.println("Address was not found"); // TODO
+				}
+			} 
+			catch (IOException e) {
+				System.err.println("Address was not found. " + e.getMessage()); // TODO
+			}
+			
+			// info sections
+			ExpandableListView list = (ExpandableListView)findViewById(R.id.details_explist);
+			SimpleExpandableListAdapter adapter = 
+				new SimpleExpandableListAdapter(
+						this,
+						createSectionsList(),	// groupData describes the first-level entries
+						R.layout.details_section_title,	// Layout for the first-level entries
+						new String[] { "title" },	// Key in the groupData maps to display
+						new int[] { R.id.details_section_title },		// Data under "colorName" key goes into this TextView
+						createChildList(),	// childData describes second-level entries
+						R.layout.details_section_content_general,	// Layout for second-level entries
+						new String[] { "content1", "content2" },	// Keys in childData maps to display
+						new int[] { R.id.details_section_content_general1, R.id.details_section_content_general2 }	// Data under the keys above go into these TextViews
+					);
+			list.setAdapter(adapter);
 		}
 		else{
 			titleView.setText("Error: no item found"); // TODO
 		}
-		
-		ExpandableListView list = (ExpandableListView)findViewById(R.id.details_explist);
-		SimpleExpandableListAdapter adapter = 
-			new SimpleExpandableListAdapter(
-					this,
-					createSectionsList(),	// groupData describes the first-level entries
-					R.layout.details_section_title,	// Layout for the first-level entries
-					new String[] { "title" },	// Key in the groupData maps to display
-					new int[] { R.id.details_section_title },		// Data under "colorName" key goes into this TextView
-					createChildList(),	// childData describes second-level entries
-					R.layout.details_section_content,	// Layout for second-level entries
-					new String[] { "content1", "content2" },	// Keys in childData maps to display
-					new int[] { R.id.details_section_content1, R.id.details_section_content2 }	// Data under the keys above go into these TextViews
-				);
-		list.setAdapter(adapter);
 	}
 		
 	private List<HashMap<String, String>> createSectionsList() {
 		ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
 		String[] sections = { 
-				"Adresse und Öffnungszeiten", 
-				"Anreise",
+				"Adresse", 
+				"Öffnungszeiten",
 				"Lageplan" };
 		for (int i = 0; i < sections.length; ++i) {
 			HashMap<String, String> m = new HashMap<String, String>();
@@ -71,12 +103,23 @@ public class DetailsActivity extends Activity {
 	private List<ArrayList<HashMap<String, ?>>> createChildList() {
 		ArrayList<ArrayList<HashMap<String, ?>>> result = new ArrayList<ArrayList<HashMap<String, ?>>>();
 
-		ArrayList<HashMap<String, ?>> addressList = new ArrayList<HashMap<String, ?>>();
-		HashMap<String, String> addressChild = new HashMap<String, String>();
-		addressChild.put("content1", "Wasserwerkstrasse 141\n8037 Zürich\nTelefon 044 362 10 80\nLeitung: Heinrich Stadler");
-		addressChild.put("content2", "Gratisbad ");
-		addressList.add(addressChild);
-		result.add(addressList);
+		ArrayList<HashMap<String, ?>> list = new ArrayList<HashMap<String, ?>>();
+		
+		// address
+		HashMap<String, String> child = new HashMap<String, String>();
+		child.put("content1", "Wasserwerkstrasse 141\n8037 Zürich\nTelefon 044 362 10 80\nLeitung: Heinrich Stadler\n\nGratisbad");
+		child.put("content2", "Tram 4/13 bis «Dammweg»\nBus 46 bis «Nordstrasse»\nS2/S8/S14 bis Bahnhof Wipkingen\nÖffentliche Parkplätze");
+		list.add(child);
+		result.add(list);
+		
+		// opening hours
+		child = new HashMap<String, String>();
+		child.put("content1", "14. Mai bis Mitte September 2011");
+		child.put("content2", "");
+		list.add(child);
+		result.add(list);
+		
+		 
 		
 //		for (int i = 0; i < 3; ++i) {
 //			// Second-level lists
