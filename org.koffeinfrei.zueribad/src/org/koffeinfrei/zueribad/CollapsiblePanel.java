f4a06a5b97ac00d2fb0d22b609f23878f38b9af9
koffeinfrei <none@none>
2010-10-09 00:39:19 +0200

    * replaced fill_parent by match_parent * added scrollview to details

--- org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/CollapsiblePanel.java
+++ org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/CollapsiblePanel.java
@@ -14,7 +14,6 @@ import android.widget.LinearLayout;
 
 public class CollapsiblePanel extends LinearLayout {
 	private int speed = 300;
-	private boolean isOpen = false;
 	private Animation collapse = null;
 	private Animation expand = null;
 
@@ -38,28 +37,15 @@ public class CollapsiblePanel extends LinearLayout {
 		TranslateAnimation anim = null;
 		AnimationSet set = new AnimationSet(true);
 
-		isOpen = !isOpen;
-
-		if (isOpen) {
-//			setVisibility(View.VISIBLE);
-//			
-//			ViewGroup.LayoutParams params = getLayoutParams();
-//			params.height = 100;//ViewGroup.LayoutParams.WRAP_CONTENT;
-//			setLayoutParams(params);
-			
-//			anim = new TranslateAnimation(0.0f, 0.0f, getLayoutParams().height * -0.5f, 0.0f);
+		if (getVisibility() == View.GONE) {
 			set.addAnimation(expand);
 			set.setAnimationListener(showListener);
 		} 
 		else {
-//			anim = new TranslateAnimation(0.0f, 0.0f, 0.0f, getLayoutParams().height * -0.5f);
-			//anim.setAnimationListener(hideListener);
 			set.addAnimation(collapse);
 			set.setAnimationListener(hideListener);
 		}
 
-		//set.addAnimation(anim);
-		//set.setDuration(speed);
 		set.setInterpolator(new AccelerateInterpolator(1.0f));
 		set.setFillAfter(true);
 		set.setFillBefore(true);
