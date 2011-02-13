e14f5c91cd3d08b785d09445f000ed6b29fd93e5
koffeinfrei <none@none>
2011-02-13 22:42:16 +0100

    * customized tab bg colors * added inactive bg color and bottom margin to search box * fixed color names

--- org.koffeinfrei.zueribad/gen/org/koffeinfrei/zueribad/R.java
+++ org.koffeinfrei.zueribad/gen/org/koffeinfrei/zueribad/R.java
@@ -33,8 +33,10 @@ containing a value of this type.
         public static final int targetHeight=0x7f010001;
     }
     public static final class color {
-        public static final int main_text_color=0x7f050000;
-        public static final int second_text_color=0x7f050001;
+        public static final int main_background=0x7f050002;
+        public static final int main_text=0x7f050000;
+        public static final int second_background=0x7f050003;
+        public static final int second_text=0x7f050001;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
@@ -52,9 +54,10 @@ containing a value of this type.
         public static final int icon=0x7f02000c;
         public static final int listselector=0x7f02000d;
         public static final int listview=0x7f02000e;
-        public static final int tab_details=0x7f02000f;
-        public static final int tab_favorites=0x7f020010;
-        public static final int tab_overview=0x7f020011;
+        public static final int searchbox=0x7f02000f;
+        public static final int tab_details=0x7f020010;
+        public static final int tab_favorites=0x7f020011;
+        public static final int tab_overview=0x7f020012;
     }
     public static final class id {
         public static final int details_homepagebutton=0x7f080003;
@@ -111,6 +114,8 @@ containing a value of this type.
         public static final int TranslucentZueribad=0x7f070000;
         public static final int ZueribadEditTextStyle=0x7f070003;
         public static final int ZueribadListViewStyle=0x7f070002;
+        /** <item name="android:background">@drawable/listview</item>
+         */
         public static final int ZueribadTabWidgetStyle=0x7f070004;
         /**  TODO define properly and remove all explicit declarations elsewhere 
          */
--- org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/ui/activities/MainTabActivity.java
+++ org.koffeinfrei.zueribad/src/org/koffeinfrei/zueribad/ui/activities/MainTabActivity.java
@@ -3,6 +3,7 @@ package org.koffeinfrei.zueribad.ui.activities;
 import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TabHost;
 import org.koffeinfrei.zueribad.R;
@@ -11,47 +12,65 @@ import org.koffeinfrei.zueribad.config.Constants;
 /**
  * This class is the actual tab container. It creates the
  * tab activities that reside inside each tab.
- * 
+ *
  * @author alexis.reigel
  *
  */
 public class
         MainTabActivity extends TabActivity {
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.main);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		createTab(
-				OverviewActivity.class, 
-				R.string.tab_title_overview, 
-				R.drawable.tab_overview);
-
-		createTab(
-				FavoritesActivity.class, 
-				R.string.tab_title_favorites, 
-				R.drawable.tab_favorites);
+        setContentView(R.layout.main);
 
         createTab(
-				DetailsActivity.class,
-				R.string.tab_title_details,
-				R.drawable.tab_details);
+                OverviewActivity.class,
+                R.string.tab_title_overview,
+                R.drawable.tab_overview);
+
+        createTab(
+                FavoritesActivity.class,
+                R.string.tab_title_favorites,
+                R.drawable.tab_favorites);
+
+        createTab(
+                DetailsActivity.class,
+                R.string.tab_title_details,
+                R.drawable.tab_details);
 
         // disable details tab on default
         getTabHost().getTabWidget().getChildTabViewAt(Constants.TAB_DETAILS_INDEX).setEnabled(false);
-	}
 
-	private void createTab(Class<? extends Activity> activityClass, int titleResourceId, int iconResourceId) {
-		TabHost tabHost = getTabHost();
-		Intent intent = new Intent(this, activityClass).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		
-		// Initialize a TabSpec for each tab and add it to the TabHost
-		TabHost.TabSpec spec = tabHost
-				.newTabSpec(activityClass.getName())
-				.setIndicator(
+        setTabColors(getTabHost());
+    }
+
+    private void createTab(Class<? extends Activity> activityClass, int titleResourceId, int iconResourceId) {
+        final TabHost tabHost = getTabHost();
+        Intent intent = new Intent(this, activityClass).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        TabHost.TabSpec spec = tabHost
+                .newTabSpec(activityClass.getName())
+                .setIndicator(
                         getText(titleResourceId),
                         getResources().getDrawable(iconResourceId))
-				.setContent(intent);
-		tabHost.addTab(spec);
-	}
+                .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                setTabColors(tabHost);
+            }
+        });
+    }
+
+    private void setTabColors(TabHost tabHost) {
+        for (int i = 0; i < tabHost.getTabWidget().getChildCount(); ++i) {
+            tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        // TODO find a way to reference from colors.xml, or put in Constants.java
+        tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(Color.parseColor("#66c0c0c0"));
+    }
 }
